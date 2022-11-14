840a0065b992862d27b6a9f63e4ea353d51a5944
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-11-14 14:46:21 +0100

    done with different props

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+
+function Message(props) {
+
+    const message = props.message
+    
+    return (
+        <p className="message">{message}</p>
+    )
+}
+
+export default Message;
+
+
---
+++ src/components/Timestamp.js
@@ -0,0 +1,10 @@
+function Timestamp(props) {
+
+    const timestamp = props.timestamp
+    
+    return (
+        <span className="timestamp">{timestamp}</span>
+    )
+}
+
+export default Timestamp;
